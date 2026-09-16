fa7a12acdaacb58c7fbc94503d8da41825b2d189
maker9100 <gang96108@gmail.com>
2026-09-16 22:06:11 +0900

    Update config.js

--- dist/config.js
+++ dist/config.js
@@ -1,2 +1,10 @@
-/* Public Firebase Web Client config only. Never paste a service-account key here. */
-window.CHEAT_ARENA_FIREBASE = null;
+window.CHEAT_ARENA_FIREBASE = {
+  apiKey: "AIzaSyD4wdo0eY9MDd1XzeNE1jXimeehqtTuXjA",
+  authDomain: "cheat-arena.firebaseapp.com",
+  databaseURL: "https://cheat-arena-default-rtdb.firebaseio.com",
+  projectId: "cheat-arena",
+  storageBucket: "cheat-arena.firebasestorage.app",
+  messagingSenderId: "899737102573",
+  appId: "1:899737102573:web:c671c289b61abce349c4e4",
+  measurementId: "G-TNSZJK1FFF"
+};
